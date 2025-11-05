6b25fc5df708c1f6cc4b1a5bb8c7e360fb2bbc76
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-11-05 22:34:32 +0200

    1 descr

--- 01-two-sum.py
+++ 01-two-sum.py
@@ -1,9 +1,25 @@
 def solve(nums, target):
+    """
+    01. Two Sum
+    -----------
+    Given an array of integers `nums` and an integer `target`, return indices of the two numbers
+    such that they add up to `target`.
+
+    Example:
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+
+    Assume exactly one solution exists, and you may not use the same element twice.
+    """
+
     existing = {}
     for i, num in enumerate(nums):
-        complaint = target - num
-        if complaint in existing:
-            return [existing[complaint], i]
+        complement = target - num
+        if complement in existing:
+            return [existing[complement], i]
         existing[num] = i
 
     raise ValueError('Solution does not exist with the given input!')
+
+if __name__ == "__main__":
+    print(solve([2, 7, 11, 15], 9))  # Expected output: [0, 1]
